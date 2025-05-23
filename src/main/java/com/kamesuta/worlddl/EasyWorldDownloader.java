badae2a352e2383160f6b942d7446f127e399342
Kamesuta <kamesuta@gmail.com>
2025-05-24 00:11:00 +0900

    shade

--- src/main/java/com/kamesuta/worlddl/EasyWorldDownloader.java
+++ src/main/java/com/kamesuta/worlddl/EasyWorldDownloader.java
@@ -19,7 +19,6 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.nio.file.Files;
 import java.text.DecimalFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
